c0392e4b1fe469a5d4260be26ffc0646e0fdca1b
sanjeevarasa-satkunarasa <ssanjeevarasa@proton.me>
2024-07-20 09:18:19 +0200

    Creating Static Components

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./SearchResults.module.css"
+
+function SearchResults () {
+    return (
+        <div className={styles.SearchResults}>
+        {/* <!-- Add a TrackList component --> */}
+      </div>
+        );
+}
+
+export default SearchResults;
---
+++ src/Components/Tracklist/Tracklist.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Tracklist.module.css"
+
+
+function Tracklist (props) {
+  function renderAction() {
+    return <button className="Track-action">{props.isRemoval? "-": "+"} </button>
+
+  }
+    return (
+        <div className={styles.TrackList}>
+        {/* <!-- You will add a map method that renders a set of Track components  --> */}
+        <li>Track 1</li>
+        <li>Track 2</li>
+        <li>Track 3</li>
+      </div>
+    );
+}
+
+export default Tracklist;
